53892dbdb8661b372efe4c61426d9146e792458e
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-21 20:35:08 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navbar from "./components/UI/navbar";
 import './App.css'
 import Home from "./pages/home";
 import Post from "./pages/posts";
+import Login from "./pages/login";
 
 function App() {
   return (
     <>
     <Navbar/>
     {/* <Home/> */}
-    <Post/>
+    {/* <Post/> */}
+    <Login/>
     </>
   );
 }
