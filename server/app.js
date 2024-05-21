f348b4d1c0e802d5e9a07c17793c16d229f3934d
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-21 22:01:58 +0530

    passport initialization

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const cookieSession = require('cookie-session')
 const express=require('express')
+const passport = require('passport')
 const app=express()
 app.use(cookieSession({
     name:"session",
@@ -7,6 +8,13 @@ app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 100
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cors({
+    origin:"http://localhost:3000/",
+    methods:"GET,POST,PUT,DELETE",
+    credentials:true,
+}))
 
 app.listen(5000,()=>{
     console.log('server is running')
