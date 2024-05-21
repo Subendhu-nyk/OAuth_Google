8b8fb0de492e430d6b3a5ffda0a5e958f27f7bb4
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-21 21:16:41 +0530

    restriction for direct access to urls using navigate

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 function App() {
+  const user=false;
   return (
     <BrowserRouter> 
     <>
@@ -15,8 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
           <Route
             path="/login"
-            element={<Login />} />   
-            <Route path="/post/:id" element={<Post/>} />       
+            element={user ? <Navigate to='/'/> :<Login/>} />   
+            <Route path="/post/:id" element={user ? <Post /> : <Navigate to="/login" />} />       
     </Routes>    
     </>
     </BrowserRouter>
