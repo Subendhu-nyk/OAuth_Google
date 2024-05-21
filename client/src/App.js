0ce7a1e69be55612d28463a72d06091559b872bb
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-21 21:11:10 +0530

    router dom config

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,23 @@ import './App.css'
 import Home from "./pages/home";
 import Post from "./pages/posts";
 import Login from "./pages/login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
+    <BrowserRouter> 
     <>
-    <Navbar/>
-    {/* <Home/> */}
-    {/* <Post/> */}
-    <Login/>
+    <Navbar/>    
+    <Routes>
+        <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={<Login />} />   
+            <Route path="/post/:id" element={<Post/>} />       
+    </Routes>    
     </>
+    </BrowserRouter>
   );
 }
 
